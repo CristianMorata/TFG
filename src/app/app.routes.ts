4c8a3afc33684f426340c8763d8c0ff04361c487
CristianMorata <cristianmorata222@gmail.com>
2025-05-30 17:57:18 +0200

    Comienzo con creación y configuración de servicio de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { CartaComponent } from './pages/carta/carta.component';
 import { MesaComponent } from './pages/mesa/mesa.component';
+import { OfertaEmpleoComponent } from './pages/oferta-empleo/oferta-empleo.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,7 @@ export const routes: Routes = [
         children: [
             { path: 'carta', component: CartaComponent },
             { path: 'mesa', component: MesaComponent },
+            { path: 'oferta-empleo', component: OfertaEmpleoComponent },
             { path: '', redirectTo: 'carta', pathMatch: 'full' }
         ]
     },
---
+++ src/app/pages/oferta-empleo/oferta-empleo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oferta-empleo',
+  imports: [],
+  templateUrl: './oferta-empleo.component.html',
+  styleUrl: './oferta-empleo.component.css'
+})
+export class OfertaEmpleoComponent {
+
+}
